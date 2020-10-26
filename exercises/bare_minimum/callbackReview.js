da6dcda94fd64cf3afe960ca67b72342694a3d53
Andy Pham <andy.pham111@gmail.com>
2020-10-26 16:48:48 -0700

    Passed Test for CB Review

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -21,8 +21,14 @@ var pluckFirstLineFromFile = function(filePath, callback) {
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-
+var getStatusCode = function (url, callback) {
+  request(url, (err, res, body) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
