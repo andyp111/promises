5d1378307f2829c601800c12180a82f3959b9382
Andy Pham <andy.pham111@gmail.com>
2020-10-27 16:06:07 -0700

    Finished Chaining

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -15,26 +15,16 @@ var asyncNode = require('./promiseConstructor.js');
 
 
 
+
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // return new Promise((resolve, reject) => {
   //   if (error) {
   //     reject(error);
   //   } else {
   return asyncNode.pluckFirstLineFromFileAsync(readFilePath)
-    .then((userName) => { return async.getGitHubProfileAsync(userName); })
-    .then((response) => {
-      fs.writeFile(writeFilePath, response, (error) => {
-        if (error) {
-          console.log('error');
-          return;
-        } else {
-          // callback(null);
-          console.log('file written successfully');
-          // return writeFilePath.concat(response);
-        }
-      });
-    })
-    .catch(console.log('got an error'));
+    .then((userName) => async.getGitHubProfileAsync(userName))
+    .then((response) =>
+      fs.writeFileSync(writeFilePath, JSON.stringify(response)));
 
   // return promise;
 
